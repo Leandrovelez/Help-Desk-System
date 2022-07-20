5514b14d429dd70863e8312ca3a801d51321cad1
Leandro Velez <leandrovelez55@gmail.com>
2022-07-19 23:18:33 -0300

    fix: fix the bg property in Signin component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -14,7 +14,7 @@ export function SignIn() {
 
     const { colors } = useTheme();
     return (
-        <VStack flex={1} alignItems="center" bg="gary.600" px={8} pt={24}>
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             {/* <Logo /> */}
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
